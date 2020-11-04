3b8759fc1008f73594d74b1b045328f4916e77b6
Marco Milanesi <kpanic@gmail.com>
2020-11-05 00:05:39 +0100

    Add tests for struct(s) with atom(s) only

--- test/carry_test.exs
+++ test/carry_test.exs
@@ -5,17 +5,35 @@ defmodule CarryTest do
     defstruct [:carillon]
   end
 
-  test "Carry.on/1" do
-    assert Carry.on(%{"carillon" => "sound"}, %Instrument{}) == %Instrument{carillon: "sound"}
+  describe "on expanded struct" do
+    test "Carry.on/1" do
+      assert Carry.on(%{"carillon" => "sound"}, %Instrument{}) == %Instrument{carillon: "sound"}
+    end
+
+    test "Carry.on/1 with keys not present in a struct" do
+      assert Carry.on(%{"carillon" => "sound", "marimba" => "percussion"}, %Instrument{}) ==
+               %Instrument{carillon: "sound"}
+    end
+
+    test "Carry.on/1 with atom values" do
+      assert Carry.on(%{"carillon" => :sound, "marimba" => :percussion}, %Instrument{}) ==
+               %Instrument{carillon: :sound}
+    end
   end
 
-  test "Carry.on/1 with keys not present in a struct" do
-    assert Carry.on(%{"carillon" => "sound", "marimba" => "percussion"}, %Instrument{}) ==
-             %Instrument{carillon: "sound"}
-  end
+  describe "on atom" do
+    test "Carry.on/1" do
+      assert Carry.on(%{"carillon" => "sound"}, Instrument) == %Instrument{carillon: "sound"}
+    end
 
-  test "Carry.on/1 with atom values" do
-    assert Carry.on(%{"carillon" => :sound, "marimba" => :percussion}, %Instrument{}) ==
-             %Instrument{carillon: :sound}
+    test "Carry.on/1 with keys not present in a struct" do
+      assert Carry.on(%{"carillon" => "sound", "marimba" => "percussion"}, Instrument) ==
+               %Instrument{carillon: "sound"}
+    end
+
+    test "Carry.on/1 with atom values" do
+      assert Carry.on(%{"carillon" => :sound, "marimba" => :percussion}, Instrument) ==
+               %Instrument{carillon: :sound}
+    end
   end
 end
