04610e6e6fe0a5639a0dcf8365421c5179fbc3e1
Marco Milanesi <kpanic@gmail.com>
2020-11-05 00:05:03 +0100

    Support struct(s) by specifying atom(s) only

--- lib/carry.ex
+++ lib/carry.ex
@@ -1,22 +1,28 @@
 defmodule Carry do
   @moduledoc """
-  Carry transforms a map to the corresponding Elixir struct:
+  Carry transforms a map into the corresponding Elixir struct without atom waste
   """
 
   @doc """
   Transform the given map to the corresponding struct
   Example:
 
-  iex> defmodule Instrument do
-    defstruct [:carillon]
-  end
+  iex> defmodule Instrument, do: defstruct [:carillon]
 
   iex> Carry.on(%{"carillon" => "sound", "marimba" => "percussion"}, %Instrument{})
   %Instrument{carillon: "sound"}
 
+  iex> Carry.on(%{"carillon" => "sound", "marimba" => "percussion"}, Instrument)
+  %Instrument{carillon: "sound"}
+
   """
-  @spec on(map(), map()) :: map()
-  def on(map, %module{} = struct) do
+  @spec on(map(), struct() | atom()) :: struct()
+  def on(map, %module{}), do: map_to_struct(map, module)
+  def on(map, module) when is_atom(module), do: map_to_struct(map, module)
+
+  defp map_to_struct(map, module) do
+    struct = struct(module)
+
     new_map =
       struct
       |> Map.from_struct()
